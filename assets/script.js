286a7e48d415a00a2a250d2501553dcfa05e5245
jvanosdol <j.vanosdol99@yahoo.com>
2022-07-29 13:12:38 -0700

    commit

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+console.log(moment());
+
+      let m = moment();
+
+      m = moment("2019-05-19");
+
+      console.log(`toString() => ${m.toString()}`);
+      console.log(`toISOString() => ${m.toISOString()}`);
+
+      localStorage.setItem('name1', 'james');
+      localStorage.setItem('name2', 'james');
+      console.log(localStorage)
+
+      let name1 = localStorage.getItem('name1');
+
+      console.log(name1);
+
+      //SADFHASBOGFHBBGFOHBUGFOahsbfdkhsdbfbadshbf
