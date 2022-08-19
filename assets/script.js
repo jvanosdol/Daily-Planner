0022a27fa1bc55a4478cdf154bf6ac840ae84da5
jvanosdol <j.vanosdol99@yahoo.com>
2022-08-19 14:33:47 -0700

    removed unused code, cleaned code up a bit

--- assets/script.js
+++ assets/script.js
@@ -1,95 +1,105 @@
-var currentTime = document.getElementById('current-time')
-var textArea = document.getElementsByClassName('text-area')
-var textArea1 = document.getElementById('text-area1')
+let currentTime = document.getElementById('current-time')
+let textArea = document.getElementsByClassName('text-area')
 
-var saveBtn1 = document.getElementById('saveBtn1')
-var deleteBtn1 = document.getElementById('deleteBtn1')
+// all text area elements
+let textArea1 = document.getElementById('text-area1')
+let textArea2 = document.getElementById('text-area2')
+let textArea3 = document.getElementById('text-area3')
+let textArea4 = document.getElementById('text-area4')
+let textArea5 = document.getElementById('text-area5')
+let textArea6 = document.getElementById('text-area6')
+let textArea7 = document.getElementById('text-area7')
+let textArea8 = document.getElementById('text-area8')
+let textArea9 = document.getElementById('text-area9')
 
-var mom = '';
+// all save button elements
+let saveBtn1 = document.getElementById('saveBtn1')
+let saveBtn2 = document.getElementById('saveBtn2')
+let saveBtn3 = document.getElementById('saveBtn3')
+let saveBtn4 = document.getElementById('saveBtn4')
+let saveBtn5 = document.getElementById('saveBtn5')
+let saveBtn6 = document.getElementById('saveBtn6')
+let saveBtn7 = document.getElementById('saveBtn7')
+let saveBtn8 = document.getElementById('saveBtn8')
+let saveBtn9 = document.getElementById('saveBtn9')
+
+// all delete button 
+let deleteBtn1 = document.getElementById('deleteBtn1')
+let deleteBtn2 = document.getElementById('deleteBtn2')
+let deleteBtn3 = document.getElementById('deleteBtn3')
+let deleteBtn4 = document.getElementById('deleteBtn4')
+let deleteBtn5 = document.getElementById('deleteBtn5')
+let deleteBtn6 = document.getElementById('deleteBtn6')
+let deleteBtn7 = document.getElementById('deleteBtn7')
+let deleteBtn8 = document.getElementById('deleteBtn8')
+let deleteBtn9 = document.getElementById('deleteBtn9')
 
 
+//**************************************************
+
+
+// assigns formatted times into variables, updates the current time every second
+let mom = '';
 
 setInterval(() => {
       mom = moment().format('LTS');
       momHr = moment().format('LT');
       currentTime.innerHTML = mom;
-
 }, 1000);
 
-
-// function styleContainers() {
-//       if ( momHr > 9 ) {
-//             textArea9.style.background = "brown";
-//       }
-// }
-
-var m = moment();
+// gets and displays current date
+let m = moment();
+let hour = moment().hours();
 $("#currentDay").text(m.format( "MMM Do, YYYY"));
 
-var hour = moment().hours();
-
-      console.log(`toString() => ${m.toString()}`);
-      console.log(`toISOString() => ${m.toISOString()}`);
-
-      localStorage.setItem('name1', 'james');
-      localStorage.setItem('name2', 'hassan');
-      console.log(localStorage)
-
-      let name1 = localStorage.getItem('name1');
-      let name2 = localStorage.getItem('name2');
-
-      console.log(name1);
-      console.log(name2);
-
-      localStorage.removeItem('name1')
-
-      var inpKey = document.querySelector('#inpKey');
-      var inpValue = document.querySelector('#inpValue');
-      var btnInsert = document.querySelector('#btnInsert');
-      var lsOutput = document.querySelector('#lsOutput');
-
-      btnInsert.addEventListener('click', () =>  {
-            let key = 'event2';
-            let value = textArea.value;
-
-            if ( key && value ) {
-                  window.localStorage.setItem(key, value);
-                  //location.reload();
-            }
-
-            let test2 = window.localStorage.getItem('event2')
-
-
-            console.log(test2)
-
-            console.log(key)
-            console.log(value)
-      });
+// logging date
+// console.log(`toString() => ${m.toString()}`);
+// console.log(`toISOString() => ${m.toISOString()}`);
 
 
 
-      saveBtn1.addEventListener('click', () =>  {
-            let key = 'textArea1';
-            let value = textArea1.value;
+btnInsert.addEventListener('click', () =>  {
+      let key = 'event2';
+      let value = textArea.value;
 
-            if ( key && value ) {
-                  window.localStorage.setItem(key, value);
-                  //location.reload();
-            }
+      if ( key && value ) {
+            window.localStorage.setItem(key, value);
+            //location.reload();
+      }
 
-            let test2 = window.localStorage.getItem('textArea1')
+      let test2 = window.localStorage.getItem('event2')
 
 
-            console.log(test2)
+      console.log(test2)
 
-            console.log(key)
-            console.log(value)
-      });
+      console.log(key)
+      console.log(value)
+});
 
-      deleteBtn1.addEventListener('click', () => {
-            window.localStorage.clear('textArea1');
-            location.reload();
-      });
+
+
+saveBtn1.addEventListener('click', () =>  {
+      let key = 'textArea1';
+      let value = textArea1.value;
+
+      if ( key && value ) {
+            window.localStorage.setItem(key, value);
+            //location.reload();
+      }
+
+      let test2 = window.localStorage.getItem('textArea1')
+
+
+      console.log(test2)
+
+      console.log(key)
+      console.log(value)
+});
+
+deleteBtn1.addEventListener('click', () => {
+      window.localStorage.clear('textArea1');
+      location.reload();
+});
 
 
 
@@ -102,14 +112,15 @@ window.onload = () => {
       console.log(hour)
 }
 
-hour = 9;
+hour = '';
 
 if ( hour < 9 ) {
-      textArea1.style.backgroundColor = 'lightgreen'
+      textArea1.style.backgroundColor = 'lightgreen';
 } else if (  hour === 9 ) {
-      textArea1.style.backgroundColor = 'coral'
+      textArea1.style.backgroundColor = 'coral';
+      textArea1.style.color = 'white';
 } else {
-      textArea1.style.backgroundColor = 'white'
+      textArea1.style.backgroundColor = 'white';
 }
 
 
@@ -128,60 +139,7 @@ if ( hour < 9 ) {
 
 
 
-// var myDiv = document.createElement("div")
 
-// function addClassName(i) {
-//       if ( i > momHr ) {
-//           textArea.style = 'red'  
-//       }
-      
-// }
-
-
-
-// var myDiv = $(".container");
-//     myDiv.html("");
-
-function createTimeSlots() {
-      for ( var i = 9; i <= 17; i++ ) {
-            
-
-
-            var time = momHr
-
-            var data = 'aasdf'
-
-               var template = `
-            <div class="row">
-                  <div id="template-container">
-                  <div class='time-slot'>
-                      ${i}AM
-                  </div>
-                  </div>
-                  <div class='past'>
-                      <textarea id='text-area${i}'>${data}</textarea>
-                  </div>
-                  <div class='save-parent'>
-                      <button class='save-btn' data-hours="${i}">SAVE</button>
-                  </div>
-            </div>
-            `;   
-            myDiv.append(template)
-            myDiv.innerHTML = '<h1>Hello World</h1>'
-            
-            console.log(myDiv)
-            console.log(time)
-            
-            //textArea[i].css.style.backgroundColor = 'red'
-
-                  //textArea.classList.add('future')
-
-            // if( i = mom ) {
-            //       $('textArea').css('background', '#ccc');
-            // }
-            
-      }
-};
 
 
 
